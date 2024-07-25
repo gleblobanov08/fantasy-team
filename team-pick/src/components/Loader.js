f72464a0de7c5ac9942a125d45d33cd62641b46f
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2024-07-25 12:48:37 +0000

    loading-animation-done

---
+++ team-pick/src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Loader () {
+    return (
+        <div className="loader">
+                <div className="load sq1"></div>
+                <div className="load sq2"></div>
+                <div className="load sq3"></div>
+        </div>
+    )
+}
+
+export default Loader;
--- team-pick/src/components/Results.js
+++ team-pick/src/components/Results.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import playerPoints from '../data/playerPoints';
+import Loader from './Loader';
 import '../styles/Components.css';
 
 function Results() {
+  const [showLoader, setShowLoader] = useState(true);
   const location = useLocation();
   const { selectedPlayers } = location.state;
   const totalPoints = selectedPlayers.reduce((total, player) => total + (playerPoints[player.name] || 0), 0);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowLoader(false);
+    }, 3000)
+}, [])
+
   return (
-    <div className='results'>
+    <div className='bg'>
+      {
+        showLoader ? (
+          <Loader />
+        ) : (
+      <div className='results'>
       <h1>Results</h1>
       <div className='table'>
         {selectedPlayers.map(player => (
@@ -24,6 +37,8 @@ function Results() {
       </div>
       <p>Total Points: {totalPoints}</p>
       <Link to="/" className='btn'>Go home</Link>
+      </div>
+      )}
     </div>
   );
 }
